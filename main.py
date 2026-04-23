27638c0ed7da7e05aece2acf701e2da61fa76998
Shhsayy <sushantkc617@gmail.com>
2026-04-23 17:09:46 +1000

    Added topper finding logic

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Student Performance Analyzer 🚀")
 class Student:
     def __init__(self, name, marks):
         self.name = name
@@ -10,6 +9,14 @@ class Student:
 
 s1 = Student("Ram", [80, 90, 100])
 s2 = Student("Sushant", [70, 85, 95])
+s3 = Student("Hari", [95, 92, 96])
 
-print(s1.name, s1.average())
-print(s2.name, s2.average())
+students = [s1, s2, s3]
+
+topper = students[0]
+
+for student in students:
+    if student.average() > topper.average():
+        topper = student
+
+print("Topper is:", topper.name, topper.average())
