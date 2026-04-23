f8b46ed122e0434ff52fb823b360e9a84e0c3b0b
Shhsayy <sushantkc617@gmail.com>
2026-04-23 17:04:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Student Performance Analyzer 🚀")
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+
+s1 = Student("Ram", [80, 90, 100])
+
+print(s1.name)
+print(s1.average())
